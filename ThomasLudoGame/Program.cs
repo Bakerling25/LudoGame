3179207260fa9c8276c61e48ab2fa7cdf568158e
Bakerling25 <thom97m6@edu.sde.dk>
2020-02-03 09:32:15 +0100

    First Commit

---
+++ ThomasLudoGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThomasLudoGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
